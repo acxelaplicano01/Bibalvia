aedba12a23737d38ebb0ba166d46f9b55b358b98
Marioo83GitHub <mcarbajalg@unah.hn>
2025-11-01 03:17:54 -0600

    agregada lista de sectores (sin datos), y ruta para detalle de cada uno

--- bivalvia/urls.py
+++ bivalvia/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')),  # ← esto conecta la raíz "/"
+    # path('admin/', admin.site.urls),
+    path('', include('dashboard.urls')),
 ]
