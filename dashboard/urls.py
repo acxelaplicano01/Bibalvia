b2aa7f3c761ed93ac8d0235885328541b0627512
Marioo83GitHub <mcarbajalg@unah.hn>
2025-11-02 18:27:20 -0600

    mapa y registro de sectores

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('sector/<int:id>/', views.sector_detail, name='sector_detail'),
+    path('sector/nuevo/', views.sector_create, name='sector_create'),
 ]
