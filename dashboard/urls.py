75a644be0308d85e3ba1db28034d860a40b3f3e3
Marioo83GitHub <mcarbajalg@unah.hn>
2025-11-18 02:19:43 -0500

    agregado manejo de imágenes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('sector/<int:id>/', views.sector_detail, name='sector_detail'),
     path('sector/nuevo/', views.sector_create, name='sector_create'),
+    path('upload-imagen/', views.upload_imagen_sector, name='upload_imagen_sector'),
+    path('borrar-imagen/', views.borrar_imagen_sector, name='borrar_imagen_sector'),
+
 ]
